ae5b6b6d41e3fdb5af6f03662d523dd9b3e4e20b
Kim Ji Eun <ys6445@naver.com>
2019-05-19 05:56:14 +0900

    BuildingFinal2로 다시 연습하기
    시설물 대여 페이지의 메뉴바를 눌러서 파싱하는 부분 해결하기
    -> 2줄로 frame "leftFrame"까지 진입성공

--- src/main/java/BuildingFinal2.java
+++ src/main/java/BuildingFinal2.java
@@ -32,5 +32,60 @@ public class BuildingFinal2 {
 		HtmlPage homepage = form.getInputByName("ibtnLogin").click();
 		System.out.println("Login Success");
 
+
+		//로그인이후에 시설물 대여 페이지 -> homepage로 정의했다.
+
+		//frame 안으로 진입하기.
+
+		//바로 아래의 공식 top 부분에 진입
+/*		HtmlPage packageListPage = (HtmlPage) homepage.getFrames().get(0).getEnclosedPage(); --> frame "topFrame"에 진입
+		packageListPage.getAnchors().get(1).click();
+
+		System.out.println(packageListPage.asXml());*/
+
+		HtmlPage packageListPage = (HtmlPage) homepage.getFrames().get(1).getEnclosedPage(); //2번째 frame "mainFrame"에 진입
+		HtmlPage packageListPage2 =(HtmlPage) packageListPage.getFrames().get(0).getEnclosedPage(); // frame "leftFrame"에 진입
+		/*packageListPage2.getAnchors().get(0).click();*/
+		/*	packageListPage.getAnchors().get(0).click();*/
+		/*List<?> divs = packageListPage2.getByXPath("//div");
+		HtmlDivision div = (HtmlDivision) packageListPage2.getElementById("//span[@class='leftmenu2']");
+		System.out.println(div.asText());*/
+		/*System.out.println(packageListPage2.asXml());*/
+
+		/*HtmlAnchor link = null;
+		for(HtmlAnchor anchor : link){
+			String str = anchor.asText();
+			if(anchor.asText().equals("[N]시설물대여 신청"));
+				link = anchor;
+		}
+
+		HtmlPage pp = link.click();*/
+
+	/*	HtmlAnchor link = packageListPage2.getFirstByXPath("//span[@class='leftmenu2']");
+		HtmlPage pagefinal = link.click();
+
+		System.out.print(pagefinal.asText());
+*/
+
+
+
+
+
+		// 문제 발생 frame 안으로 진입 불가
+		/*HtmlForm webService1 = homepage.getFormByName("Left_Menu");*/
+
+	/*	HtmlSubmitInput button = (HtmlSubmitInput) webService1.getInputsByName("a");
+		HtmlTextInput textField = webService1.getInputByName("");
+
+		HtmlPage webService2 = button.click();
+		System.out.println(webService2.asText());*/
+
+		/*List<?> divs = homepage.getByXPath("//div"); // div로 시작되는거 전부 입력 받기
+		for(int i=0; i<3; i++){
+			System.out.println(divs.get(i));
+		}*/
+		//HtmlDivision div = (HtmlDivision) page.getByXPath("//div[@class='MenuDiv']")); //그 중 div의 클래스 이름이 MenuDiv인 것 받기.
+		//System.out.println(div.asText());
+
 	}
 }
